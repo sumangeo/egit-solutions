857b824a780ccb8110789e7b2d7833e306ecda64
Suman <35394894+sumangeo@users.noreply.github.com>
2024-01-16 17:12:46 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,29 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>EGIT Solutions!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome!" />
-        <p className="description">
-         <a href="https://egit.netlify.app" target="_blank">Visit EGIT Solutions</a>
-        </p>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src="/logo.png" 
+            alt="EGIT Solutions Logo"
+            style={{
+              maxWidth: '50px', 
+              height: 'auto', 
+              marginRight: '10px', 
+            }}
+            onClick={() => window.open('https://egit.netlify.app', '_blank')}
+          />
+          <p style={{ fontSize: '20px' }}>
+            <a href="https://egit.netlify.app" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
+              Visit EGIT Solutions
+            </a>
+          </p>
+        </div>
       </main>
 
       <Footer />
