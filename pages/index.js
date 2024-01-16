a26186181423bc4867dc215e2b65dc84277b97fd
Suman <35394894+sumangeo@users.noreply.github.com>
2024-01-16 16:52:55 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         <a href="https://egit.netlify.app" target="_blank">Visit EGIT Solutions</a>
         </p>
       </main>
 
